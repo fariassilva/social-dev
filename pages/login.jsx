94ef241d6b806df1f177552bf242df162cab64f4
fariassilva <alessandrofarias405@gmail.com>
2023-01-14 09:19:56 -0300

    Componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,14 @@
+
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+function LoginPage (){
+
+  return(
+
+    <ImageWithSpace>
+      teste 123
+    </ImageWithSpace>
+
+  )
+}
+export default LoginPage
