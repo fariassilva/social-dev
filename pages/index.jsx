b2ac6c5601eb7b7a6e0be3a5620fc7c3a3fb492f
fariassilva <alessandrofarias405@gmail.com>
2023-01-18 20:22:39 -0300

    Estrutura da página

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,14 @@
+import styled from 'styled-components'
+
 import Navbar from "../src/components/layout/Navbar"
+import Container from '../src/components/layout/Container'
+import CreatePost from '../src/components/cards/CreatePost'
+
+const Content = styled.div`
+margin: 50px 0;
+
+
+`
 
 function HomePage () {
   
@@ -6,9 +16,14 @@ function HomePage () {
     <>
     <Navbar/>
    
-    <div>
-      SejaDev
-    </div>
+    <Content>
+      <Container>
+
+      <CreatePost/>
+
+      </Container>
+      
+    </Content>
 
     </>
   )
