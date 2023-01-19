5c60b09c501a1aba5f6e5641062c6b556e80bc06
fariassilva <alessandrofarias405@gmail.com>
2023-01-19 13:50:19 -0300

    Posts

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -4,6 +4,11 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
 display: flex;
 justify-content: center;
+
+@media (max-width: 700px){
+
+  padding: 10px;
+}
 `
 
 const StyledChildren = styled.div`
